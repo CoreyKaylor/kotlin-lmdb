1715e2617670dc440c48fa7dc896338bcc090197
Corey Kaylor <corey@kaylors.net>
2023-04-16 10:43:15 -0400

    Initial project commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    kotlin("multiplatform") version "1.8.0"
+}
+
+group = "com.crowdedinnovations"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(11)
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
